a40d2b06c93dcfa5721b6b80e54eedad15ebdbb5
nickie kane <kameo712@gmail.com>
2019-12-09 19:02:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,22 +1,5 @@
 def square_array(array)
- newArray = (array.collect{|n| n ** 2})
-  print newArray
-  puts "\n"
-  
-end
-
-numbers = [1,2,3]
- 
-square_array(numbers)
- 
-new_numbers = [9,10,16,25]
- 
-square_array(new_numbers)
-
-def square_array(array)
-  newArray = []
- newArray = (array.collect{|array| array**2})
-  print newArray
+ newArray = (array.each{|n| puts n ** 2})
   puts "\n"
   
 end
