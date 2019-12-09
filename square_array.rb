aa6ed827cfe3c2183a3c238ae99ad6282fab9785
nickie kane <kameo712@gmail.com>
2019-12-09 19:18:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,13 @@
 def square_array(array)
- newArray = (array.each{|item| puts item ** 2})
-  puts "\n"
-  
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
 
 numbers = [1,2,3]
  
-square_array(numbers)
+print square_array(numbers)
  
 new_numbers = [9,10,16,25]
  
-square_array(new_numbers)
+print square_array(new_numbers)
